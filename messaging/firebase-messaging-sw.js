860854c794e2903864057f1fba8a216f09fbc539
s-kuhn <dev+github@s-kuhn.de>
2024-07-01 13:52:56 +0200

    remove sensitive data

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,15 +1,15 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
-importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
+// importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
+// importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
+// importScripts('/__/firebase/init.js');
+//
+// const messaging = firebase.messaging();
 
-const messaging = firebase.messaging();
 
-/**
- * Here is is the code snippet to initialize Firebase Messaging in the Service
- * Worker when your app is not hosted on Firebase Hosting.
+ // * Here is is the code snippet to initialize Firebase Messaging in the Service
+ // * Worker when your app is not hosted on Firebase Hosting.
 
  // Give the service worker access to Firebase Messaging.
  // Note that you can only use Firebase Messaging here. Other Firebase libraries
@@ -21,20 +21,13 @@ const messaging = firebase.messaging();
  // your app's Firebase config object.
  // https://firebase.google.com/docs/web/setup#config-object
  firebase.initializeApp({
-   apiKey: 'api-key',
-   authDomain: 'project-id.firebaseapp.com',
-   databaseURL: 'https://project-id.firebaseio.com',
-   projectId: 'project-id',
-   storageBucket: 'project-id.appspot.com',
-   messagingSenderId: 'sender-id',
-   appId: 'app-id',
-   measurementId: 'G-measurement-id',
+   // TODO: insert here
  });
 
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
  const messaging = firebase.messaging();
- **/
+
 
 
 // If you would like to customize notifications that are received in the
